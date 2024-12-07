9efac9dd782a542c35e0246aa7229b53ccc3744c
ChenJo20 <chenjo20@oocl.com>
2024-12-07 12:39:44 +0800

    feat: add GlobalExceptionHandler

---
+++ src/main/java/org/afs/pakinglot/domain/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package org.afs.pakinglot.domain.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoAvailablePositionException.class)
+    public ResponseEntity<String> handleNoAvailablePositionException(NoAvailablePositionException ex) {
+        return new ResponseEntity<>("No available positions", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UnrecognizedTicketException.class)
+    public ResponseEntity<String> handleUnrecognizedTicketException(UnrecognizedTicketException ex) {
+        return new ResponseEntity<>("Unrecognized ticket", HttpStatus.BAD_REQUEST);
+    }
+}
