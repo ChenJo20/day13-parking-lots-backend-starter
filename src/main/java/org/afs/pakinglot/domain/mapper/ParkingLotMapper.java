dd71938e44511327d37964af8f9fdf796c26d9c5
ChenJo20 <chenjo20@oocl.com>
2024-12-07 13:40:11 +0800

    feat: update the return dto

--- src/main/java/org/afs/pakinglot/domain/mapper/ParkingLotMapper.java
+++ src/main/java/org/afs/pakinglot/domain/mapper/ParkingLotMapper.java
@@ -1,22 +1,36 @@
 package org.afs.pakinglot.domain.mapper;
 
 import org.afs.pakinglot.domain.ParkingLot;
+import org.afs.pakinglot.domain.Ticket;
 import org.afs.pakinglot.domain.dto.ParkingLotDTO;
 import org.afs.pakinglot.domain.dto.TicketDTO;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class ParkingLotMapper {
 
     public ParkingLotDTO toParkingLotDTO(ParkingLot parkingLot) {
+        List<TicketDTO> ticketDTOs = new ArrayList<>();
+        for (int i = 1; i <= parkingLot.getCapacity(); i++) {
+            int finalI = i;
+            Ticket ticket = parkingLot.getTickets().stream()
+                    .filter(t -> t.position() == finalI)
+                    .findFirst()
+                    .orElse(null);
+            if (ticket != null) {
+                ticketDTOs.add(new TicketDTO(ticket.position(), ticket.plateNumber(), ticket.parkingLot()));
+            } else {
+                ticketDTOs.add(new TicketDTO(i, "", parkingLot.getId()));
+            }
+        }
         return new ParkingLotDTO(
                 parkingLot.getId(),
                 parkingLot.getName(),
-                parkingLot.getTickets().stream()
-                        .map(ticket -> new TicketDTO(ticket.position(), ticket.plateNumber(), ticket.parkingLot()))
-                        .collect(Collectors.toList())
+                ticketDTOs
         );
     }
 }
